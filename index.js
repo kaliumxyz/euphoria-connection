bb49b7e06f1435af3b6cdb21df28ab62c66d3cea
kalium <martijn@becker.wtf>
2017-05-28 03:10:28 +0200

    Redid everything to match with instant-connection.

--- index.js
+++ index.js
@@ -1,107 +1,85 @@
 'use strict'
-const WebSocket = require('ws')
-const EventEmitter = require('events')
-let logger = require('k-log')
-logger = new logger('euphoria.log', true, false)
+const ws = require('ws')
 
-class connection extends EventEmitter {
-	constructor(room = 'test', human = 0, logging = false, ...callback) {
-		super()
-		const ws = new WebSocket(`wss://euphoria.io/room/${room}/ws?h=${human}`, {
-			origin: 'https://euphoria.io'
-		})
+class connection extends ws {
+	constructor(room = 'test', human = 0, uri = "wss://euphoria.io", options = { origin: 'https://euphoria.io' }, ...callback) {
+		super(`${uri}/room/${room}/ws?h=${human}`, options)
 
-		this.ws = ws
-		this.resetListeners()
-		this.ws.on('open', data => {
-			this.log('opened: ', data)
-			this.call(...callback)
+		// Setting the basics for the connection
+		this.on('open', data => {
+			callback.forEach(f => f(data))
 			this.emit('ready')
+
+			this.on('message', this.handleMsg)
+			// Setting default behaviour keep-open behaviour.
+			this.on('ping-event', data => {
+				this.send(`{
+				"type": "ping",
+				"data": {"time": ${data.data.time}}
+				}`)
+			})
 		})
 
 	}
 
-
-	resetListeners() {
-		let that = this
-		this.ws.removeAllListeners()
-
-		this.ws.on('close', (data, pew) => {
-			this.log('closed: ', data, pew)
-		})
-
-		this.ws.on('error', err => {
-			this.log(err)
-		})
-
-		this.ws.on('message', data => {
-			this.log(data)
-			let dt = JSON.parse(data)
-			if (dt.type === "ping-event") {
-				that.send("ping-reply", { "time": dt.data.time })
-			}
-		})
-	}
-	send(type, data) {
-		this.log(data)
-
-		if (data)
-			data = ', "data":' + JSON.stringify(data)
-		else
-			data = ''
-		this.ws.send(`{
-		"type": "${type}"
-		${data}
-		}`)
+	handleMsg(data, flags) {
+		const dt = JSON.parse(data)
+		this.emit(dt.type, dt)
 	}
 
-	download(number = 100, before, ...callback) {
-		if (before)
-			this.send("log", { n: number, "before": before })
-		else
-			this.send("log", { n: number })
-		this.call(...callback)
+	download(number = 100, before = null, ...callback) {
+		this.send(`{
+			"type": "log",
+			"data": {"n": ${number}, "before": "${before}" }
+			}`)
+		this.once('log-reply', data => {
+			callback.forEach(f => f(data))
+		})
 	}
 
+/*
 	downloadAll(downloadCallback, ...callback) {
 		this.download(1000)
-		this.ws.on('message', data => {
-		if (callback) {
-			this.ws.on('message', data => {
-				let dt = JSON.parse(data)
-				if (dt.type === "log-reply")
-					// Only do the callback if you actually have stuff to return.
-					if (dt.data.log[0]){
-						let parent = dt.data.log[0].id
-						dt.data.log.forEach(item => downloadCallback(JSON.stringify(item) + '\n'))
-						setTimeout(_ => this.download(1000, parent), 1000)
-					} else {
-						
-						downloadCallback('end\n')
-						this.resetListeners()
-						this.call(...callback)
-					}
-			})
-		}
+		this.once('log-reply', data => {
+			if (callback) {
+				this.ws.on('message', data => {
+					let dt = JSON.parse(data)
+					if (dt.type === "log-reply")
+						// Only do the callback if you actually have stuff to return.
+						if (dt.data.log[0]) {
+							let parent = dt.data.log[0].id
+							dt.data.log.forEach(item => downloadCallback(JSON.stringify(item) + '\n'))
+							setTimeout(_ => this.download(1000, parent), 1000)
+						} else {
+
+							downloadCallback('end\n')
+							this.resetListeners()
+							callback.forEach(f => f(data))
+						}
+				})
+			}
+		})
+	}
+*/
+
+	nick(nick = '<><', ...callback) {
+		this.send(`{
+		"type": "nick",
+		"data": {"name": "${nick}"}
+		}`)
+		this.once('nick-reply', data => {
+			callback.forEach(f => f(data))
 		})
 	}
 
-	nick(nick = '<><') {
-		this.send('nick', { "name": nick })
-		return nick
-	}
-
-	log(data) {
-		logger.log(data)
-	}
-
-	call(...callback) {
-		if (callback)
-			try {
-				callback.forEach(e => e())
-			} catch (e) {
-				console.error(`Your callback function(s) threw the following error: `, e)
-			}
+	post(text, parent, ...callback){
+		this.send(`{
+		"type": "send",
+		"data": {"content": "${text}", "parent": ${parent}}
+		}`)
+		this.once('send-reply', data => {
+			callback.forEach(f => f(data))
+		})
 	}
 }
 
--- test.js
+++ test.js
@@ -4,32 +4,55 @@ const connection = require('./')
 test('can create connection',t => {
 	t.true(new connection('test') instanceof connection)
 })
+
 test('can connect', async t => {
 	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection('test', 0, false, resolve)
+		const testConnection = new connection('test')
+		testConnection.once('ready', _ => resolve())
 	})
 
 	t.is(await testPromise)
 })
 
-test('download', async t => {
+test('can set nick', async t => {
 	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection('test', 0, false, callback)
-		function callback () {
-			testConnection.download(100, false, _ => resolve())
-		}
+		const testConnection = new connection('test')
+		testConnection.once('ready', _ => {
+			testConnection.nick('><>', _ => resolve())
+		})
 	})
 
 	t.is(await testPromise)
 })
 
-test('downloadAll', async t => {
+test('can download', async t => {
 	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection('test', 0, false, callback)
-		function callback () {
-			testConnection.downloadAll( _ => _ , resolve)
-		}
+		const testConnection = new connection('test')
+		testConnection.once('ready', _ => {
+			testConnection.download(1000, null, _ => resolve())
+		})
 	})
 
 	t.is(await testPromise)
 })
+
+test('can post', async t => {
+	const testPromise = new Promise((resolve, reject) => {
+		const testConnection = new connection('test')
+		testConnection.once('ready', _ => {
+			testConnection.post("pew", null, _ => resolve())
+		})
+	})
+
+	t.is(await testPromise)
+})
+// test('downloadAll', async t => {
+// 	const testPromise = new Promise((resolve, reject) => {
+// 		const testConnection = new connection('test', 0, false, callback)
+// 		function callback () {
+// 			testConnection.downloadAll( _ => _ , resolve)
+// 		}
+// 	})
+
+// 	t.is(await testPromise)
+// })
