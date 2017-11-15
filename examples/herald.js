3b9744fd6432d5bf18b742f2c31a68f3d609401f
kalium <martijn@kalium.xyz>
2017-11-15 18:05:09 +0100

    feat(example) add herald.js

---
+++ examples/herald.js
@@ -0,0 +1,17 @@
+'use strict';
+/* libs */
+const Connection = require('../')
+
+/* config */
+const room = "test"
+
+// instantiate the connection with the room
+const connection = new Connection(room)
+// set our funcitonal code on the ready event, so we are sure that the socket has been created and is connected to the server
+connection.once('ready', _ => {
+	// on any join event greet the person joining
+	connection.nick('><>')
+	connection.on('join-event', ev => {
+		connection.post(`greetings ${ev.data.name || "new user"}`)
+	})
+})
