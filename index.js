6cf97d80594b7f2e006a514ab538f7f3b6f2c21b
kaliumxyz <kalium@kalium.xyz>
2020-04-16 16:35:08 +0900

    feat/fix: major update read description
    fixes:
     - memory leak due to close attached to process.on("exit")
     - identity to _identity in tests
     - refractoring
    
    features:
     - add login
     - add register
     - add grant-manager
     - add PM
     - add persistent sessions trough cookies

--- index.js
+++ index.js
@@ -2,143 +2,153 @@
 const ws = require('ws');
 
 class connection extends ws {
-	constructor(room = 'test', human = 0, uri = 'wss://euphoria.io', options = { origin: 'https://euphoria.io' }, ...callback) {
-		super(`${uri}/room/${room}/ws?h=${human}`, options);
+    constructor(room = 'test', human = 0, uri = 'wss://euphoria.io', options = { origin: 'https://euphoria.io' }, ...callback) {
+        options.origin = options.origin ? options.origin : 'https://euphoria.io';
+        super(`${uri}/room/${room}/ws?h=${human}`, options);
 
-		// Setting the basics for the connection
-		this.on('open', () => {
-			this.on('message', this.handleMsg);
-			// Setting default behaviour keep-open behaviour.
-			this.on('ping-event', data => {
-				// any heartbeat should be riding on this event
-				this.send(JSON.stringify({
-					type: 'ping-reply',
-					data: {time: data.data.time}
-				}));
-			});
-		});
+        this.cookie = '';
+        this.once('upgrade', res => {
+            this.cookie = res.headers['set-cookie'][0];
+        });
+        // Setting the basics for the connection
+        this.on('open', () => {
+            this.on('message', this.handleMsg);
+            // Setting default behaviour keep-open behaviour.
+            this.on('ping-event', data => {
+                // any heartbeat should be riding on this event
+                this.send(JSON.stringify({
+                    type: 'ping-reply',
+                    data: {time: data.data.time}
+                }));
+            });
+        });
 
-		// The snapshot event will happen right after opening and delivers relevant information, as such we can use it for the callback. Should not be a once because it also happens on reconnect.
-		this.on('snapshot-event', raw => {
-			const data = raw.data;
-			this._identity = data.identity;
-			this._version = data.version;
-			this._session_id = data.session_id;
-			this.emit('ready');
-			callback.forEach(f => f(raw));
-		});
+        // The snapshot event will happen right after opening and delivers relevant information, as such we can use it for the callback. Should not be a once because it also happens on reconnect.
+        this.on('snapshot-event', raw => {
+            const data = raw.data;
+            this._identity = data.identity;
+            this._version = data.version;
+            this._session_id = data.session_id;
+            this.emit('ready');
+            callback.forEach(f => f(raw));
+        });
+    }
 
-		process.on('beforeExit', () => {
-			this.close();
-		});
+    handleMsg(data) {
+        // credit to ArkaneMoose https://github.com/ArkaneMoose/EuphoriaJS/blob/master/connection.js#L19
+        // formated as https://github.com/websockets/ws/blob/5d90141505dc41c129ab5d5228e37d49979d7541/lib/event-target.js#L87
+        // console.log(data);
+        let dt = {};
+        try {
+            dt = JSON.parse(data);
+        } catch (e) {
+            this.emit('error', new Error('invalid JSON'));
+        }
+        if(dt.type)
+            this.emit(dt.type, dt);
+        else
+            this.emit('error', new Error('missing type'));
+    }
 
-	}
-
-	handleMsg(data) {
-		// credit to ArkaneMoose https://github.com/ArkaneMoose/EuphoriaJS/blob/master/connection.js#L19
-		// formated as https://github.com/websockets/ws/blob/5d90141505dc41c129ab5d5228e37d49979d7541/lib/event-target.js#L87
-		try {
-			const dt = JSON.parse(data);
-			if(dt.type)
-				this.emit(dt.type, dt);
-			else
-				this.emit('error', new Error('missing type'));
-			return;
-		} catch (e) {
-			this.emit('error', new Error('invalid JSON'));
-		}
-	}
-
-	who(...callback) {
-		this.send(JSON.stringify({
-			type: 'who',
-		}));
-		this.once('who-reply', data => {
-			callback.forEach(f => f(data));
-		});
-	}
-
-
-	download(number = 100, before = null, ...callback) {
-		this.once('log-reply', data => {
-			callback.forEach(f => f(data));
-			number -= number % 1000;  
-			if(data.before && number)
-				this.download(number-1000, data.before, ...callback);
-		});
-		this.send(JSON.stringify({
-			type: 'log',
-			data: {
-				n: number % 1000 ? number % 1000 : number,
-				before: before ? before : void(0)
-			}
-		}));
-	}
+    who(...callback) {
+        this.send(JSON.stringify({
+            type: 'who',
+        }));
+        this.once('who-reply', data => {
+            callback.forEach(f => f(data));
+        });
+    }
 
 
-	downloadAll(downloadCallback, ...callback) {
-		this.download(100, null, downloadCallback);
-		this.on('log-reply', recurse);
-		function recurse(data){
-			if(data.data.log[0]){
-				this.download(1000, data.data.log[0].id, downloadCallback);
-			} else {
-				() => this.removeListener('log-reply', recurse);
-				callback.forEach(f => f(data));
-			}
-		}
-	}
+    download(number = 100, before = null, ...callback) {
+        this.once('log-reply', data => {
+            callback.forEach(f => f(data));
+            number -= number % 1000;
+            if(data.before && number)
+                this.download(number-1000, data.before, ...callback);
+        });
+        this.send(JSON.stringify({
+            type: 'log',
+            data: {
+                n: number % 1000 ? number % 1000 : number,
+                before: before ? before : void(0)
+            }
+        }));
+    }
 
-	nick(nick = '<><', ...callback) {
-		this.send(JSON.stringify({
-			type: 'nick',
-			data: {name: nick}
-		}));
-		this.once('nick-reply', data => {
-			callback.forEach(f => f(data));
-		});
-	}
 
-	pm(user_id, ...callback) {
-		this.send(JSON.stringify({
-			type: 'pm-initiate',
-			data: {user_id: user_id}
-		}));
-		this.once('nick-reply', data => {
-			callback.forEach(f => f(data));
-		});
-	}
+    downloadAll(downloadCallback, ...callback) {
+        this.download(100, null, downloadCallback);
+        this.on('log-reply', recurse);
+        function recurse(data){
+            if(data.data.log[0]){
+                this.download(1000, data.data.log[0].id, downloadCallback);
+            } else {
+                () => this.removeListener('log-reply', recurse);
+                callback.forEach(f => f(data));
+            }
+        }
+    }
 
-	post(text, parent, ...callback) {
-		this.send(JSON.stringify({
-			type: 'send',
-			data: {content: text, parent: parent}
-		}));
-		this.once('send-reply', data => {
-			callback.forEach(f => f(data));
-		});
-	}
+    nick(nick = '<><', ...callback) {
+        this.send(JSON.stringify({
+            type: 'nick',
+            data: {name: nick}
+        }));
+        this.once('nick-reply', data => {
+            callback.forEach(f => f(data));
+        });
+    }
 
-	login(namespace, id, password, callback) {
-		this.send(JSON.stringify({
-			type: 'login',
-			data: {namespace: namespace, id: id, password: password}
-		}));
-		this.once('login-reply', data => {
-			callback.forEach(f => f(data));
-		});
-	
-	}
+    pm(user_id, ...callback) {
+        this.send(JSON.stringify({
+            type: 'pm-initiate',
+            data: {user_id: user_id}
+        }));
+        this.once('pm-initiate-reply', data => {
+            callback.forEach(f => f(data));
+        });
+    }
 
-	registerAccount(namespace, id, password, callback) {
-		this.send(JSON.stringify({
-			type: 'send',
-			data: {content: text, parent: parent}
-		}));
-		this.once('send-reply', data => {
-			callback.forEach(f => f(data));
-		});
-	}
+    grant_manager(user_id, ...callback) {
+        this.send(JSON.stringify({
+            type: 'grant-manager',
+            data: {user_id: user_id}
+        }));
+        this.once('grant-manager-reply', data => {
+            callback.forEach(f => f(data));
+        });
+    }
+
+    post(text, parent, ...callback) {
+        this.send(JSON.stringify({
+            type: 'send',
+            data: {content: text, parent: parent}
+        }));
+        this.once('send-reply', data => {
+            callback.forEach(f => f(data));
+        });
+    }
+
+    login(namespace, id, password, ...callback) {
+		    this.send(JSON.stringify({
+			      type: 'login',
+			      data: {namespace: namespace, id: id, password: password}
+		    }));
+		    this.once('login-reply', data => {
+			      callback.forEach(f => f(data));
+		    });
+	  }
+
+	  registerAccount(namespace, id, password, ...callback) {
+		    this.send(JSON.stringify({
+			      type: 'register-account',
+			      data: {namespace: namespace, id: id, password: password}
+		    }));
+		    this.once('register-account-reply', data => {
+			      callback.forEach(f => f(data));
+		    });
+	  }
 }
 
 module.exports = connection;
--- test.js
+++ test.js
@@ -1,173 +1,231 @@
-import test from 'ava'
+import test from 'ava';
 import { setInterval } from 'timers';
-const connection = require('./')
+const connection = require('./');
 
 const config = {
 	room: 'test'
-}
+};
 
 test('can create connection',t => {
-	t.true(new connection(config.room) instanceof connection)
-})
+	t.true(new connection(config.room) instanceof connection);
+});
 
 test('can connect', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => resolve())
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => resolve());
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can reconnect', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		let testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection = new connection(config.room)
-			testConnection.once('ready', _ => resolve())
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		let testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection = new connection(config.room);
+			testConnection.once('ready', () => resolve());
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can set nick', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.nick('><>', _ => resolve())
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection.nick('><>', () => resolve());
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can download', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.download(1000, null, _ => resolve())
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection.download(1000, null, () => resolve());
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can download multiple requests', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.download(2000, null, _ => resolve())
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection.download(2000, null, () => resolve());
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can download all', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.downloadAll( _ => _, _ => resolve())
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection.downloadAll( _ => _, () => resolve());
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can post', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.nick('><>')
-			testConnection.post('pew', null, _ => resolve())
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection.nick('can post');
+			testConnection.post('post', null, () => resolve());
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
+
+test('can post with parent', async t => {
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection.nick('can post with parent');
+			testConnection.post('parent post', null, json => {
+				testConnection.post('child post', json.data.id, () => resolve());
+			});
+		});
+	});
+
+	t.is(await testPromise);
+});
 
 test('can lurk for 2 minutes', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.nick()
-			setTimeout( _ => {
-				resolve()
-			}, 1000 * 60 * 2)
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			testConnection.nick();
+			setTimeout( () => {
+				resolve();
+			}, 1000 * 60 * 2);
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can lurk for 2 minutes while changing nick every second', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			let i = 120
-			testConnection.nick('120')
-			setInterval( _ => {
-				testConnection.nick('' + --i)
-			}, 1000)
-			setTimeout( _ => {
-				resolve()
-			}, 1000 * 60 * 2)
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			let i = 120;
+			testConnection.nick('120');
+			setInterval( () => {
+				testConnection.nick('' + --i);
+			}, 1000);
+			setTimeout( () => {
+				resolve();
+			}, 1000 * 60 * 2);
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test('can get a userID', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			resolve(testConnection.identity)
-		})
-	})
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			resolve(testConnection._identity);
+		});
+	});
 
-	t.is(typeof await testPromise, 'string')
-})
+	t.is(typeof await testPromise, 'string');
+});
 
-test('can initiate a PM', async t => {
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.once('pm-initiate-event', _ => resolve())
-			testConnection.pm(testConnection.identity)
-		})
-	})
+test('can request to register an account', async t => {
+	const credentials = require('./creds.json');
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection('pmtest');
+		testConnection.once('ready', () => {
+        testConnection.nick('narwal', () => {
+			      testConnection.registerAccount('',  credentials.name, credentials.password);
+			      testConnection.once('register-account-reply', () => {
+				        resolve();
+			      });
+        });
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
 
 test.skip('can register an account', async t => {
-	const credentials = require('./creds.json')
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.registerAccount(testConnection.session_id, testConnection.identity, 'narwall')
-			testConnection.once('register-account-reply', _ => {
-				resolve()
-			})
-		})
-	})
+	  const credentials = require('./creds.json');
+	  const testPromise = new Promise(resolve => {
+		    const testConnection = new connection('pmtest');
+		    testConnection.once('ready', () => {
+            testConnection.nick('narwal', () => {
+                testConnection.post('test', () => {
+                    testConnection.registerAccount('email',  credentials.name, credentials.password, reply => {
+                        console.log(reply);
+                        resolve();
+                    });
+                });
+            });
+		    });
+	  });
 
-	t.is(await testPromise)
-})
+	  t.is(await testPromise);
+});
 
-test.skip('can login to an account', async t => {
-	const credentials = require('./creds.json')
-	const testPromise = new Promise((resolve, reject) => {
-		const testConnection = new connection(config.room)
-		testConnection.once('ready', _ => {
-			testConnection.login(testConnection.session_id, testConnection.identity, 'narwall')
-			testConnection.once('register-account-reply', _ => {
-				resolve()
-			})
-		})
-	})
+test('can login to an account', async t => {
+    const credentials = require('./creds.json');
+	const testPromise = new Promise(resolve => {
+		const testConnection = new connection(config.room);
+		testConnection.once('ready', () => {
+			  testConnection.login('email',  credentials.email, credentials.password, reply => {
+				    resolve();
+        });
+		});
+	});
 
-	t.is(await testPromise)
-})
+	t.is(await testPromise);
+});
+
+test('can login to an account and switch IDs', async t => {
+    const credentials = require('./creds.json');
+    const testPromise = new Promise(resolve => {
+        const testConnection = new connection('pmtest');
+        testConnection.once('ready', () => {
+            testConnection.login('email',  credentials.email, credentials.password, reply => {
+                const newConnection = new connection('pmtest', 0, 'wss://euphoria.io', {headers: {Cookie: testConnection.cookie}});
+                newConnection.once('ready', () => {
+                    newConnection.nick('narwal');
+                    setTimeout(resolve,1000);
+                });
+            });
+        });
+    });
+
+    t.is(await testPromise);
+});
+
+test.only('can initiate a PM', async t => {
+    const credentials = require('./creds.json');
+    const testPromise = new Promise(resolve => {
+        const testConnection = new connection('pmtest');
+        testConnection.once('ready', () => {
+            testConnection.login('email',  credentials.email, credentials.password, reply => {
+                const newConnection = new connection('pmtest', 0, 'wss://euphoria.io', {headers: {Cookie: testConnection.cookie}});
+                newConnection.once('ready', () => {
+                    console.log(newConnection._identity)
+                    newConnection.pm(newConnection._identity, console.log);
+                    setTimeout(resolve,1000);
+                });
+            });
+        });
+    });
+	  t.is(await testPromise);
+});
